9c1ba6e3863d492da2b20e178380ad3b1e7beb19
Aman Bhanse <amanbhanse22@gmail.com>
2021-12-12 11:54:41 +0530

    Add thunk to app

--- Note.md
+++ Note.md
@@ -6,3 +6,9 @@ Command : npm install redux-persist if your app is crashing after you persist th
 delete the local storage of persist. In chrome, go to dev tools, application tab, under application
 tab->At left section -> storage -> local Storage ->underneath local storage->local host , select it
 -> then at right section , select persist and delete it
+
+### thunk
+
+command : npm install redux-thunk redux-devtools-extension @babel/runtime
+
+npm install --save-dev @babel/plugin-transform-runtime
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { todos } from "./todos/reducers";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import automergerLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = {
     todos,
@@ -19,7 +21,4 @@ const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistCongfig, rootReducer);
 
 export const configureStore = () =>
-    createStore(
-        persistedReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+    createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));

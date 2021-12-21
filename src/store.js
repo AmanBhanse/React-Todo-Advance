e19b1051d437f95631088ae1c413a1db8ab9063a
Aman Bhanse <amanbhanse22@gmail.com>
2021-12-21 19:46:00 +0530

    Refactoring : Combined isloading reducer and todos reducer into one reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import { todos, isLoading } from "./todos/reducers";
+import { todos } from "./todos/reducers";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import automergerLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
@@ -8,7 +8,6 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = {
     todos,
-    isLoading,
 };
 
 const persistCongfig = {
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -7,53 +7,61 @@ import {
     LOAD_TODOS_FAILURE,
 } from "./actions";
 
-export const isLoading = (state = false, action) => {
-    const { type } = action;
-    switch (type) {
-        case LOAD_TODOS_IN_PROGRESS:
-            return true;
-        case LOAD_TODOS_SUCCESS:
-            return false;
-        case LOAD_TODOS_FAILURE:
-            return false;
-        default:
-            return state;
-    }
-};
+const initialState = { isLoading: false, data: [] };
 
-export const todos = (state = [], action) => {
+export const todos = (state = initialState, action) => {
     const { type, payload } = action;
 
     switch (type) {
         case CREATE_TODO: {
             const { todo } = payload;
 
-            return state.concat(todo);
+            return {
+                ...state,
+                data: state.data.concat(todo),
+            };
         }
 
         case REMOVE_TODO: {
             const { todoID } = payload;
-            return state.filter((todo) => todo.id !== todoID);
+            return {
+                ...state,
+                data: state.data.filter((todo) => todo.id !== todoID),
+            };
         }
 
         case MARK_TODO_COMPLETE: {
             const { todoID } = payload;
 
-            return state.map((todo) => {
-                if (todo.id === todoID) {
-                    return { ...todo, isCompleted: true };
-                }
-                return todo;
-            });
+            return {
+                ...state,
+                data: state.data.map((todo) => {
+                    if (todo.id === todoID) {
+                        return { ...todo, isCompleted: true };
+                    }
+                    return todo;
+                }),
+            };
         }
         case LOAD_TODOS_SUCCESS: {
             const { todos } = payload;
-            return todos;
+            return {
+                ...state,
+                isLoading: false,
+                data: todos,
+            };
         }
 
         case LOAD_TODOS_IN_PROGRESS:
+            return {
+                ...state,
+                isLoading: true,
+            };
         case LOAD_TODOS_FAILURE:
         default:
-            return state;
+            return {
+                ...state,
+                isLoading: false,
+            };
     }
 };
